085531755ca74e3c942880055895cf82055d668e
Eddy Togni <info@eddyt-togni.com>
2021-10-24 15:31:42 +0200

    Update useStorage hook

--- src/modules/piano/hooks/useStorage.ts
+++ src/modules/piano/hooks/useStorage.ts
@@ -8,6 +8,13 @@ function tryParse(value: string) {
   }
 }
 
+function parse(value: any) {
+  if (typeof value === 'object') {
+    return JSON.stringify(value);
+  }
+  return value;
+}
+
 // @TODO: improve
 export const useStorate = <S>(key: string, defaultValue: S): [S, Dispatch<SetStateAction<S>>] => {
   const [state, setState] = useState<S>(
@@ -17,10 +24,10 @@ export const useStorate = <S>(key: string, defaultValue: S): [S, Dispatch<SetSta
   const setStorage = (argument: any) => {
     if (typeof argument === 'function') {
       const returnValue = argument(state);
-      localStorage.setItem(key, JSON.stringify(returnValue));
+      localStorage.setItem(key, parse(returnValue));
       setState(returnValue);
     } else {
-      localStorage.setItem(key, JSON.stringify(argument));
+      localStorage.setItem(key, parse(argument));
       setState(argument);
     }
   };
