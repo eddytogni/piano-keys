5b8fbc6dad6755d07db9fc81b0b5ac56729413c1
Eddy Togni <info@eddyt-togni.com>
2021-10-24 13:12:43 +0200

    offset Sharp note to the left

--- src/modules/piano/components/Staff/Svg/Sharp.styled.tsx
+++ src/modules/piano/components/Staff/Svg/Sharp.styled.tsx
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 export const Svg = styled.svg(
   ({ theme: { utils } }) => css`
     height: ${utils.em(28)};
+    margin-left: ${utils.em(-13 - 4)};
     margin-right: ${utils.em(4)};
     width: ${utils.em(13)};
   `
